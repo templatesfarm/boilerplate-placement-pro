78d777cf4c402b8ab3268f9b499050868ffcde9b
DeepakPahawa <deepak.pahawa1911@gmail.com>
2024-11-30 14:24:30 +0530

    eslint fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar/Navbar";
 export default function Footer() {
   return (
     <div className="border-t-[1px] py-5 bg-gray-950">
-      <Navbar className="flex-col gap-5" />
+      <Navbar />
     </div>
   );
 }
