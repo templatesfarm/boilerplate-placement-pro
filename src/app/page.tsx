aac919a7c048171c158daf06e3688ccd04d8caf8
DeepakPahawa <deepak.pahawa1911@gmail.com>
2024-12-04 02:46:47 +0530

    adding a message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
@@ -8,14 +8,18 @@ import Skills from "@/components/Skills/Skills";
 import { useAppStore } from "@/store/appStore";
 
 export default function Home() {
-
-  const {isEditing} = useAppStore();
+  const { isEditing } = useAppStore();
   return (
     <div className="min-h-screen bg-black overflow-hidden">
       <div className="max-w-7xl mx-auto p-5">
         {!isEditing && <PasswordInput />}
+        {isEditing && (
+          <div className="text-center mx-auto text-blue-500 animate-pulse">
+            {`Just Refresh the page once you're done with the changes`}
+          </div>
+        )}
         <Navbar />
-        <Hero/>
+        <Hero />
         <Skills />
         <Projects />
       </div>
