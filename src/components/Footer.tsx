b25b6a972bedb9cb0fce642b03cfd5669707beb9
DeepakPahawa <deepak.pahawa1911@gmail.com>
2024-12-04 02:11:12 +0530

    Footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,9 @@ import Navbar from "./Navbar/Navbar";
 export default function Footer() {
   return (
     <div className="border-t-[1px] py-5 bg-gray-950">
-      <Navbar />
+      <div className="max-w-7xl mx-auto p-5">
+        <Navbar />
+      </div>
     </div>
   );
 }
