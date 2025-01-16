1697cdd65243e128b3593aa277a4e13e33b450fd
DeepakPahawa <deepak.pahawa1911@gmail.com>
2025-01-16 22:12:10 +0530

    Portfolio Completed

--- src/components/TextGenerateEffectDemo.tsx
+++ src/components/TextGenerateEffectDemo.tsx
@@ -2,5 +2,7 @@
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 export function TextGenerateEffectDemo({ words }: { words: string }) {
-  return <TextGenerateEffect words={words} className="w-[60%] mx-auto" />;
+  return (
+    <TextGenerateEffect words={words} className="w-[80%] md:w-[60%] mx-auto" />
+  );
 }
